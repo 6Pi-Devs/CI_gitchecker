97805e788af5cb33c12949cbfee8af6f4c5c5959
Torre <yupipi93@gmail.com>
2020-11-03 22:52:47 +0100

    auto-commit

--- gitChecker.sh
+++ gitChecker.sh
@@ -61,22 +61,4 @@ main(){
 
 }
 main
-=======
-#!/bin/sh
 
-UPSTREAM=${1:-'@{u}'}
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "$UPSTREAM")
-BASE=$(git merge-base @ "$UPSTREAM")
-
-if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date"
-elif [ $LOCAL = $BASE ]; then
-    echo "Need to pull"
-elif [ $REMOTE = $BASE ]; then
-    echo "Need to push"
-else
-    echo "Diverged"
-fi
-#comment
->>>>>>> 66bfb333ad9fd00b97608609dcd93e938de89360
